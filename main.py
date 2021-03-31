4aec5dc6d6aa91e9dfd8a08e9df40cdedf10a7a4
Darshil Mehta <darshilm223@gmail.com>
2021-03-31 13:49:26 +0530

    voice assistant integrated with tkinter

--- main.py
+++ main.py
@@ -2,59 +2,75 @@ import speech_recognition as sr
 import webbrowser
 from datetime import datetime
 import yfinance as yf
+import playsound
+import os
+import random
+from gtts import gTTS
+import time
 r = sr.Recognizer()
 
 
 def record_audio(ask=""):
     with sr.Microphone() as source:
         if ask:
-            print(ask)
+            speak(ask)
         audio = r.listen(source, phrase_time_limit=4)
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
         except sr.UnknownValueError:
-            print('I did not get that')
+            speak('I did not get that')
         except sr.RequestError:
-            print('Sorry, the service is down')
+            speak('Sorry, the service is down')
         return voice_data.lower()
+
+
+def speak(audio_string):
+    tts = gTTS(text=audio_string, lang="en")
+    randomNum = random.randint(1, 10000000)
+    audio_file = 'audio-' + str(randomNum) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
 
 
 def response(user_command):
     if 'how are you' in user_command:
-        print('I am fine, thanks for asking!')
+        speak('I am fine, thanks for asking!')
     elif 'what is your name' in user_command:
-        print("Hello I am Alpha")
+        speak("Hello I am Alpha")
     elif 'search google' in user_command:
         search_google = record_audio("What do you want to search for on google?")
         url = 'https://google.com/search?q=' + search_google
         webbrowser.get().open(url)
-        print("Here is what i found for " + search_google)
+        speak("Here is what i found for " + search_google)
     elif 'search youtube' in user_command:
         search_youtube = record_audio("What do you want to search for on youtube?")
         url = 'https://www.youtube.com/results?search_query=' + search_youtube
         webbrowser.get().open(url)
-        print("Here is what i found for " + search_youtube)
+        speak("Here is what i found for " + search_youtube)
     elif 'open mail' in user_command:
         url = 'https://mail.google.com/mail/u/0/#inbox'
         webbrowser.get().open(url)
+        speak("Your inbox is now opened")
     elif 'find location' in user_command:
         location = record_audio("What location do you want to find on maps?")
         url = 'https://www.google.co.in/maps/place/' + location
         webbrowser.get().open(url)
-        print("Here is your required location " + location)
+        speak("Here is your required location " + location)
     elif 'find direction' in user_command:
         current_location = record_audio("Where are you currently?")
         final_destination = record_audio("Where do you want to go?")
         url = 'https://www.google.co.in/maps/dir/' + current_location + '/' + final_destination
         webbrowser.get().open(url)
-        print("Here is how you can reach " + final_destination + " from " + current_location)
+        speak("Here is how you can reach " + final_destination + " from " + current_location)
     elif 'what is the time' in user_command:
         current_time = datetime.now().time()
-        print("Currently the time is " + str(current_time))
+        speak("Currently the time is " + str(current_time))
     elif 'what is the date' in user_command:
         current_date = datetime.now().date()
-        print("Today is " + str(current_date))
+        speak("Today is " + str(current_date))
     elif 'get stock details' in user_command:
         search_term = record_audio("Which stock do u want to search for?")
         stocks = {
@@ -87,7 +103,7 @@ def response(user_command):
             stock = yf.Ticker(stock)
             price = stock.info["regularMarketPrice"]
             currency = stock.info["currency"]
-            print("Market Price of " + search_term + " is " + str(price) + " " + currency)
+            speak("Market Price of " + search_term + " is " + str(price) + " " + currency)
         except:
             print("No data available for the stock you are searching for!")
     elif 'convert currency' in user_command:
@@ -127,23 +143,16 @@ def response(user_command):
             if currency_two == "pakistani rupee":
                 currency_two_value = value * 2.1276595
         else:
-            print("No data found for the mentioned currencies")
+            speak("No data found for the mentioned currencies")
         currency_two_values = "{:.2f}".format(currency_two_value)
-        print(str(currency_one_value) + " " + currency_one + " is equal to " + str(currency_two_values) + " " + currency_two)
-    elif 'find word meaning' in user_command:
+        speak(str(currency_one_value) + " " + currency_one + " is equal to " + str(currency_two_values) + " " + currency_two)
+    elif 'dictionary' in user_command:
         word = record_audio("Which word meaning do you want?")
         url = "https://www.dictionary.com/browse/" + word
         webbrowser.get().open(url)
-        print("Here is what " + word + " means!")
+        speak("Here is what " + word + " means!")
     elif 'exit' in user_command:
-        print('going offline')
+        speak('going offline')
         exit()
     else:
-        print("I did not get what you want me to do. Please repeat again!")
-
-
-while(True):
-    print("How can i help you?")
-    user_command = record_audio()
-    print(user_command)
-    response(user_command)
+        speak("I did not get what you want me to do. Please repeat again!")
--- test.py
+++ test.py
@@ -1,20 +1,29 @@
 from tkinter import *
+from main import *
 
 root = Tk()
 root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
-root.geometry("600x400")
+root.geometry("540x330")
 
 header_font = ("Castellar", 15, "bold italic")
 body_font = ("Castellar", 12)
-feature_font = ("Castellar", 11, "underline")
+feature_font = ("Castellar", 11)
 
 
 def show_frame(frame):
     frame.tkraise()
 
 
-frame_mainPage = Frame(root)
+def start_alpha():
+    while True:
+        time.sleep(1)
+        speak("How can i help you?")
+        user_command = record_audio()
+        response(user_command)
+
+
+frame_mainPage = Frame(root, bg="#2E81F4")
 frame_aboutPage = Frame(root, bg="#B4EDD2")
 frame_startPage = Frame(root, bg="#89023E")
 
@@ -28,92 +37,105 @@ start_header_one = Label(frame_startPage,
                          font=header_font,
                          pady=10,
                          bg="#89023E",
-                         fg="white").pack()
+                         fg="white").grid(row=0, column=0, columnspan=5)
 start_header_two = Label(frame_startPage,
                          text="This is your personal voice assistant!",
                          font=body_font,
                          pady=10,
                          bg="#89023E",
-                         fg="white").pack()
+                         fg="white").grid(row=1, column=0, columnspan=5)
 start_header_three = Label(frame_startPage,
                            text="Click on 'Use me' to get information regarding anything using voice command",
                            font=body_font,
                            pady=10,
                            bg="#89023E",
-                           fg="white").pack()
+                           fg="white").grid(row=2, column=0, columnspan=5)
 btn_mainPage = Button(frame_startPage,
                       text="Use me!",
                       bg="#EA638C",
                       fg="white",
                       pady=8,
-                      command=lambda: show_frame(frame_mainPage)).pack()
+                      command=lambda: show_frame(frame_mainPage)).grid(row=3, column=1, sticky="nsew")
 btn_aboutPage = Button(frame_startPage,
                        text="About me!",
                        bg="#EA638C",
                        fg="white",
                        pady=8,
-                       command=lambda: show_frame(frame_aboutPage)).pack()
+                       command=lambda: show_frame(frame_aboutPage)).grid(row=3, column=3, sticky="nsew")
 
 """ about page """
 start_header = Label(frame_aboutPage,
                      text="What things can i do for you?",
                      font=header_font,
-                     padx=10,
+                     pady=5,
                      bg="#B4EDD2").pack()
 my_features = Label(frame_aboutPage,
-                    text="Search anything on google!",
+                    text="Search anything on google",
                     font=feature_font,
                     bg="#B4EDD2",
-                    padx=7).pack()
+                    pady=3).pack()
 my_features = Label(frame_aboutPage,
-                    text="Search anything on youtube!",
+                    text="Search anything on youtube",
                     font=feature_font,
                     bg="#B4EDD2",
-                    padx=7).pack()
+                    pady=3).pack()
 my_features = Label(frame_aboutPage,
                     text="Find location",
                     font=feature_font,
                     bg="#B4EDD2",
-                    padx=7).pack()
+                    pady=3).pack()
 my_features = Label(frame_aboutPage,
                     text="Find a route from a place to another (Google Maps)",
                     font=feature_font,
                     bg="#B4EDD2",
-                    padx=7).pack()
+                    pady=3).pack()
 my_features = Label(frame_aboutPage,
                     text="Get stock prices of some trending stocks",
                     font=feature_font,
                     bg="#B4EDD2",
-                    padx=7).pack()
+                    pady=3).pack()
 my_features = Label(frame_aboutPage,
                     text="Convert currency",
                     font=feature_font,
                     bg="#B4EDD2",
-                    padx=7).pack()
+                    pady=3).pack()
 my_features = Label(frame_aboutPage,
                     text="Get current time and date",
                     font=feature_font,
                     bg="#B4EDD2",
-                    padx=7).pack()
+                    pady=3).pack()
 my_features = Label(frame_aboutPage,
                     text="Find meaning of any word from dictionary",
                     font=feature_font,
                     bg="#B4EDD2",
-                    padx=7).pack()
+                    pady=3).pack()
 my_features = Label(frame_aboutPage,
                     text="Access your emails",
                     font=feature_font,
                     bg="#B4EDD2",
-                    padx=7).pack()
+                    pady=3).pack()
 btn_homePage = Button(frame_aboutPage,
-                      text="Back",
+                      text="<< Back",
                       bg="#87677B",
                       fg="white",
                       command=lambda: show_frame(frame_startPage)).pack()
 
 """ main page """
-start_header = Label(frame_mainPage, text="Click below on start button to use me!").pack()
-btn_start_voice = Button(frame_mainPage, text="START").pack()
-btn_homePage = Button(frame_mainPage, text="Back",fg="white", command=lambda: show_frame(frame_startPage)).pack()
+start_header = Label(frame_mainPage,
+                     font=header_font,
+                     bg="#2E81F4",
+                     fg="white",
+                     pady=10,
+                     text="Click below on start button to use me!").grid(row=0, column=0, columnspan=5, sticky="e")
+btn_homePage = Button(frame_mainPage,
+                      text="<< Back",
+                      bg="#87677B",
+                      fg="white",
+                      command=lambda: show_frame(frame_startPage)).grid(row=1, column=1, sticky="nsew")
+btn_start_voice = Button(frame_mainPage,
+                         bg="#2376BA",
+                         fg="white",
+                         command=start_alpha,
+                         text="START").grid(row=1, column=3, sticky="nsew")
 
 root.mainloop()
